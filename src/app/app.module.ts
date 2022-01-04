81ea6f07fdcf9b25caa81210f4498177b9279180
sachinPTS <sachin@pureecosoft.com>
2022-01-05 00:28:52 +0530

    * component created - header, footer, main, quote. * app.module.ts updated with new component configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule
