8c3704728c90c955800072725f59a2198f6c08fa
sachinPTS <sachin@pureecosoft.com>
2022-01-05 09:07:59 +0530

    looping quote component at parent level sending data to child component from parent component i.e. @input

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quote',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+@Input() quote;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.quote)
   }
 
 }
